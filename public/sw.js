1c248311108854f5b5ce6465a6a36f506ca1b863
Mostafa Ahmed <mostafa.ahmed1111177@gmail.com>
2026-08-21 23:32:56 +0300

    Simplify app icon

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sixth-preparatory-v2';
+const CACHE_NAME = 'sixth-preparatory-v3';
 const APP_SHELL = ['/', '/manifest.webmanifest', '/app-icon.svg', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (event) => {
